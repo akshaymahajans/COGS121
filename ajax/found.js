55b5c9dbaec8054401ff0f5c8486de4b5db92e54
Prince Le <princevietle@gmail.com>
2017-05-09 20:12:43 -0700

    Trying to uniquely assign all ids

--- ajax/found.js
+++ ajax/found.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 /* TODO: Implement the Found AJAX Handler */
 module.exports = {
   post: function (req, res){
+  	req.body.price = parseInt(req.body.price);
     req.body.lat = parseFloat(req.body.lat);
     req.body.lng = parseFloat(req.body.lng);/*user will enter a string which we needs to convert to float*/
-    require("./data.json").push(req.body);/*push is a function in array that can add a new data to the end of array*/
+
+    var d = require("./data.json");
+    d.push(req.body);
+    fs.writeFile("ajax/data.json", JSON.stringify(d));
     res.success();
   }
 }
