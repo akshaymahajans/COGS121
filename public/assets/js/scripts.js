ea7fc8a520acc7d5fc366a019847c8c2d2dfb91b
Siru A <s1a@ucsd.edu>
2017-05-17 12:36:08 -0700

    Siru UI stuff

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,20 +1,20 @@
 
 jQuery(document).ready(function() {
-	
+
     /*
         Fullscreen background
     */
-    $.backstretch("assets/img/backgrounds/1.jpg");
-    
+    //$.backstretch("img/BP.png");
+
     /*
         Form validation
     */
     $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
     });
-    
+
     $('.login-form').on('submit', function(e) {
-    	
+
     	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
     		if( $(this).val() == "" ) {
     			e.preventDefault();
@@ -24,8 +24,26 @@ jQuery(document).ready(function() {
     			$(this).removeClass('input-error');
     		}
     	});
-    	
+
     });
-    
-    
+
+		$('.login-form-blue input[type="text"], .login-form-blue input[type="password"], .login-form textarea').on('focus', function() {
+			$(this).removeClass('input-error-blue');
+		});
+
+		$('.login-form-blue').on('submit', function(e) {
+
+			$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+				if( $(this).val() == "" ) {
+					e.preventDefault();
+					$(this).addClass('input-error-blue');
+				}
+				else {
+					$(this).removeClass('input-error-blue');
+				}
+			});
+
+		});
+
+
 });
