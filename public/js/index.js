cc0ca148c95f4a3d653dd9c9599e11ee077ec77b
Siru A <s1a@ucsd.edu>
2017-05-31 14:53:22 -0700

    can save one promotion

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,7 @@ $(document).ready()
                       data.forEach(function (sale) {
                         $("#collist").append(
                             '<li><div class="collapsible-header"><i collapsiblelass="material-icons"></i>'
-                            +sale.bName+
+                            +'<b>'+sale.bName+'</b>'+
                             '<i class="material-icons starbtn"  style="float: right;">star</i></div><div id="promotionCell" class="collapsible-body">'
                             +'&nbsp;<b>Business Type:</b>'+sale.bType
                             +'</br>&nbsp;<b>Promotion Location:</b>'+sale.loc
